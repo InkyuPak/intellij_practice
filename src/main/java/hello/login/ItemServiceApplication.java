1c8b91f1c45993ba27a683f96e4de80c1c4fd4f1
InKyuPak <zzz563214@gmail.com>
2023-06-07 15:49:06 +0900

    first commit

---
+++ src/main/java/hello/login/ItemServiceApplication.java
@@ -0,0 +1,13 @@
+package hello.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication.class, args);
+	}
+}
